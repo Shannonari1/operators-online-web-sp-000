68bc13f456daeed074b5c71054b61f5995742eec
Shannon Young <shannon.ari1@gmail.com>
2020-12-12 01:55:24 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,4 +1,5 @@
 def unsafe?(speed)
+<<<<<<< HEAD
 	if speed > 60 || speed < 40
 			return true;
 		else
@@ -7,4 +8,14 @@ def unsafe?(speed)
 		end
 def not_safe?(speed)
 	 speed < 40 || speed > 60 ? true : false
+=======
+if  speed < 40 || speed > 60
+
+end
+
+
+
+def not_safe?(speed)
+	if speed < 40 || speed > 60 ? true : false
+>>>>>>> cecdab373d3f0caa83a853c6c06f73750079619d
 end
